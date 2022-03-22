95331126d3b351cfe465e7ac7862b5e6a7a89286
Fernando Silva <fernando457829@gmail.com>
2022-03-22 14:25:23 +0000

    feat: add obsidian

--- homes/fernando/default.nix
+++ homes/fernando/default.nix
@@ -5,6 +5,7 @@
     ./firefox
     ./fish
     ./git
+    ./obsidian
     ./qutebrowser
     ./starship
     ./xmonad
---
+++ homes/fernando/obsidian/default.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  programs.obsidian = {
+    enable = true;
+  };
+}
