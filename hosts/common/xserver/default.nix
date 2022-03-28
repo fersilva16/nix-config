a77ff5268ee8647d37f35231734b176b9ab45393
Fernando Silva <fernandonsilva16@gmail.com>
2022-03-27 23:48:34 -0300

    feat: add xsession session

--- hosts/common/xserver/default.nix
+++ hosts/common/xserver/default.nix
@@ -7,30 +7,16 @@
 
     # videoDrivers = [ "intel" "nvidia" ];
 
-    # displayManager = {
-    #   defaultSession = "xsession";
+    displayManager = {
+      defaultSession = "xsession";
 
-    #   session = [
-    #     {
-    #       name = "xsession";
-    #       manage = "desktop";
-    #       start = ''exec $HOME/.xsession'';
-    #     }
-    #   ];
-
-    #   startx.enable = true;
-    # };
-
-    # Not sure if I need xmonad here when I'm using xsession
-    # windowManager.xmonad = {
-    #   enable = true;
-    #   enableContribAndExtras = true;
-    #   extraPackages = haskellPackages: with haskellPackages; [
-    #     xmonad
-    #     xmonad-contrib
-    #     xmonad-extras
-    #     xmobar
-    #   ];
-    # };
+      session = [
+        {
+          name = "xsession";
+          manage = "desktop";
+          start = ''exec $HOME/.xsession'';
+        }
+      ];
+    };
   };
 }
