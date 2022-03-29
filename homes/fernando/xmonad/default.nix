492ac2406b2338d368baa521b251a16797070e52
Fernando Silva <fernandonsilva16@gmail.com>
2022-03-29 20:12:46 -0300

    feat: add xrandr commands to xsession init

--- homes/fernando/xmonad/default.nix
+++ homes/fernando/xmonad/default.nix
@@ -3,6 +3,11 @@
   xsession = {
     enable = true;
 
+    initExtra = ''
+      xrandr --output eDP-1-1 --mode 1920x1080 --rate 144 --primary
+      xrandr --output HDMI-0 --mode 1920x1080  --rate 60 --left-of eDP-1-1
+    '';
+
     windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
