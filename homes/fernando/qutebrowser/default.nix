0d55ed8f0c519e6271d8f52acf5ef2b1505810b3
Fernando Silva <fernandonsilva16@gmail.com>
2022-03-30 10:47:58 -0300

    feat: add qutebrowser as default

--- homes/fernando/qutebrowser/default.nix
+++ homes/fernando/qutebrowser/default.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ lib, ... }:
 {
   programs.qutebrowser = {
     enable = true;
   };
+
+  xdg.mimeApps.defaultApplications = lib.genAttrs [
+    "text/html"
+    "text/xml"
+    "x-scheme-handler/http"
+    "x-scheme-handler/https"
+    "x-scheme-handler/qute"
+  ] (_: [ "org.qutebrowser.qutebrowser.desktop" ]);
 }
