061d30cae90383ab7922f7dd797c4cb8063670cd
Fernando Silva <fernandonsilva16@gmail.com>
2022-03-29 22:03:00 -0300

    feat: add grub with windows

--- hosts/common/boot/default.nix
+++ hosts/common/boot/default.nix
@@ -3,13 +3,36 @@
   boot = {
     loader = {
       timeout = 10;
-      efi.canTouchEfiVariables = true;
-
-      systemd-boot = {
-        enable = true;
-        consoleMode = "max";
-        editor = false;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
       };
+
+      # Using grub because of the dualboot with Windows
+      # TODO: move it to another file
+      grub = {
+        enable = true;
+        version = 2;
+
+        efiSupport = true;
+
+        extraEntries = ''
+          menuentry "Windows" {
+            insmod part_gpt
+            insmod fat
+            insmod search_fs_uuid
+            insmod chain
+            search --fs-uuid --set=root 3436-097E
+            chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+          }
+        '';
+      };
+
+      # systemd-boot = {
+      #   enable = true;
+      #   consoleMode = "max";
+      #   editor = false;
+      # };
     };
   };
 }
