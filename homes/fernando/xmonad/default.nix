de7138a0278856136fe4c83f5339ac9e4e687324
Fernando Silva <fernando457829@gmail.com>
2022-04-05 11:27:12 -0300

    feat: add keyctl link

--- homes/fernando/xmonad/default.nix
+++ homes/fernando/xmonad/default.nix
@@ -6,7 +6,9 @@
     initExtra = ''
       xrandr --output eDP-1-1 --mode 1920x1080 --rate 144 --primary
       xrandr --output HDMI-0 --mode 1920x1080  --rate 60 --left-of eDP-1-1
-    '';
+
+      keyctl link @u @s
+   '';
 
     windowManager.xmonad = {
       enable = true;
