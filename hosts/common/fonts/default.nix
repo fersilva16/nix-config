7e00eb95f68e02a270068d177b7dc5648ad1dc6d
Fernando Silva <fernando457829@gmail.com>
2022-04-05 17:50:25 -0300

    feat: update default fonts

--- hosts/common/fonts/default.nix
+++ hosts/common/fonts/default.nix
@@ -5,7 +5,16 @@
     fonts = with pkgs; [
       noto-fonts
       twitter-color-emoji
-      nerdfonts
+      (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
     ];
+
+    fontconfig = {
+      defaultFonts = {
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+        monospace = [ "Caskadia Cove Nerd Font" ];
+        emoji = [ "Twitter Color Emoji" ];
+      };
+    };
   };
 }
