d32bdc98d4dc00b150feef51526ccb42792549f0
Fernando Silva <fernando457829@gmail.com>
2022-04-05 15:52:40 -0300

    feat: update kitty-themes

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,4 +4,15 @@ let
 in
 {
   nvidia-vaapi-driver = lib.hiPrio super.nvidia-vaapi-driver;
+
+  kitty-themes = super.kitty-themes.overrideAttrs
+    (oldAttrs: {
+      version = "2022-04-05";
+      src = self.fetchFromGitHub {
+        owner = "kovidgoyal";
+        repo = oldAttrs.pname;
+        rev = "0cc90555e3725b193785bc7e5266b27db5c08b2b";
+        sha256 = "lq+VE/+BL7F0SKmNaXMKm/4cM9NVARKJzJThyLG5Ec4=";
+      };
+    });
 }
