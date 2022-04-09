0fd83b625d0a88f92cf23aa2c637984fdeae7cd3
Fernando Silva <fernando457829@gmail.com>
2022-04-09 20:43:50 -0300

    feat(xsession): update primary monitor scale and DPI

--- homes/fernando/xmonad/default.nix
+++ homes/fernando/xmonad/default.nix
@@ -3,8 +3,8 @@
     enable = true;
 
     initExtra = ''
-      xrandr --output eDP-1-1 --mode 1920x1080 --rate 144 --primary
-      xrandr --output HDMI-0 --mode 1920x1080  --rate 60 --left-of eDP-1-1
+      xrandr --output eDP-1-1 --mode 1920x1080 --rate 144 --dpi 120 --scale 0.8x0.8 --primary
+      xrandr --output HDMI-0 --mode 1920x1080  --rate 60 --dpi 96 --left-of eDP-1-1
 
       keyctl link @u @s
     '';
