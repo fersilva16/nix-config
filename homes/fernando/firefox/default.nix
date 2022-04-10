df51135015df9839a9e69b820df914db81838f9f
Fernando Silva <fernando457829@gmail.com>
2022-04-10 12:28:43 -0300

    feat(firefox): add default profile

--- homes/fernando/firefox/default.nix
+++ homes/fernando/firefox/default.nix
@@ -3,5 +3,7 @@
     enable = true;
 
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [ ublock-origin ];
+
+    profiles = { default = { settings = { "app.update.auto" = false; }; }; };
   };
 }
