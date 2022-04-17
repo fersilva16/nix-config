9df690687ce4fb18d6907d2ddaac97a12e9cfb74
Fernando Silva <fernando457829@gmail.com>
2022-04-17 15:34:53 -0300

    feat(emacs): use native compilation

--- homes/fernando/emacs/emacs.nix
+++ homes/fernando/emacs/emacs.nix
@@ -3,7 +3,7 @@
   programs.doom-emacs = {
     enable = true;
     doomPrivateDir = ./doom.d;
-    emacsPackage = pkgs.emacsUnstable;
+    emacsPackage = pkgs.emacsGcc;
   };
 
   services.emacs = {
