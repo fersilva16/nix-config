73f8f4bb67f9a332463c06fc1ffd2702fae7e19a
Fernando Silva <fernandonsilva16@gmail.com>
2022-04-24 12:22:59 -0300

    fix(vscode-extensions): update script

--- pkgs/my-vscode-extensions/update.sh
+++ pkgs/my-vscode-extensions/update.sh
@@ -20,37 +20,37 @@ set -eu -o pipefail
 
 # Helper to just fail with a message and non-zero exit code.
 function fail() {
-    echo "$1" >&2
-    exit 1
+  echo "$1" >&2
+  exit 1
 }
 
 # Helper to clean up after ourselves if we're killed by SIGINT.
 function clean_up() {
-    TDIR="${TMPDIR:-/tmp}"
-    echo "Script killed, cleaning up tmpdirs: $TDIR/vscode_exts_*" >&2
-    rm -Rf "$TDIR/vscode_exts_*"
+  TDIR="${TMPDIR:-/tmp}"
+  echo "Script killed, cleaning up tmpdirs: $TDIR/vscode_exts_*" >&2
+  rm -Rf "$TDIR/vscode_exts_*"
 }
 
 function get_vsixpkg() {
-    N="$1.$2"
+  N="$1.$2"
 
-    # Create a tempdir for the extension download.
-    EXTTMP=$(mktemp -d -t vscode_exts_XXXXXXXX)
+  # Create a tempdir for the extension download.
+  EXTTMP=$(mktemp -d -t vscode_exts_XXXXXXXX)
 
-    URL="https://$1.gallery.vsassets.io/_apis/public/gallery/publisher/$1/extension/$2/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+  URL="https://$1.gallery.vsassets.io/_apis/public/gallery/publisher/$1/extension/$2/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
 
-    # Quietly but delicately curl down the file, blowing up at the first sign of trouble.
-    curl --silent --show-error --retry 3 --fail -X GET -o "$EXTTMP/$N.zip" "$URL"
-    # Unpack the file we need to stdout then pull out the version
-    VER=$(jq -r '.version' <(unzip -qc "$EXTTMP/$N.zip" "extension/package.json"))
-    # Calculate the SHA
-    SHA=$(nix-hash --flat --base32 --type sha256 "$EXTTMP/$N.zip")
+  # Quietly but delicately curl down the file, blowing up at the first sign of trouble.
+  curl --silent --show-error --retry 3 --fail -X GET -o "$EXTTMP/$N.zip" "$URL"
+  # Unpack the file we need to stdout then pull out the version
+  VER=$(jq -r '.version' <(unzip -qc "$EXTTMP/$N.zip" "extension/package.json"))
+  # Calculate the SHA
+  SHA=$(nix-hash --flat --base32 --type sha256 "$EXTTMP/$N.zip")
 
-    # Clean up.
-    rm -Rf "$EXTTMP"
-    # I don't like 'rm -Rf' lurking in my scripts but this seems appropriate.
+  # Clean up.
+  rm -Rf "$EXTTMP"
+  # I don't like 'rm -Rf' lurking in my scripts but this seems appropriate.
 
-    cat <<-EOF
+  cat <<-EOF
   {
     name = "$2";
     publisher = "$1";
@@ -62,14 +62,14 @@ EOF
 
 # See if we can find our `code` binary somewhere.
 if [ $# -ne 0 ]; then
-    CODE=$1
+  CODE=$1
 else
-    CODE=$(command -v code || command -v codium)
+  CODE=$(command -v code || command -v codium)
 fi
 
 if [ -z "$CODE" ]; then
-    # Not much point continuing.
-    fail "VSCode executable not found"
+  # Not much point continuing.
+  fail "VSCode executable not found"
 fi
 
 # Try to be a good citizen and clean up after ourselves if we're killed.
@@ -81,10 +81,11 @@ printf '[\n'
 # Note that we are only looking to update extensions that are already installed.
 for i in $(cat extensions)
 do
-    OWNER=$(echo "$i" | cut -d. -f1)
-    EXT=$(echo "$i" | cut -d. -f2)
+  OWNER=$(echo "$i" | cut -d. -f1)
+  EXT=$(echo "$i" | cut -d. -f2)
 
-    get_vsixpkg "$OWNER" "$EXT"
+  get_vsixpkg "$OWNER" "$EXT"
 done
+
 # Close off the nix expression.
-printf ']'
+printf ']\n'
