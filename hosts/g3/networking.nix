b5855416cfe8390aecd5c575bde69dd2f69d4874
Fernando Silva <fernandonsilva16@gmail.com>
2022-04-25 14:18:06 -0300

    feat(networking): add extra hosts

--- hosts/g3/networking.nix
+++ hosts/g3/networking.nix
@@ -8,4 +8,12 @@
       useDHCP = true;
     };
   };
+
+  networking.extraHosts = ''
+    127.0.0.1 company1
+    127.0.0.1 company2
+    127.0.0.1 company3
+    127.0.0.1 company4
+    127.0.0.1 company5
+  '';
 }
