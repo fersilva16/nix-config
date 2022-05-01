da49fa8a06879884eb043e4be8034e0af4acb618
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-01 10:41:22 -0300

    feat(vscode): symlink configs out of store

--- users/common/vscode/vscode.nix
+++ users/common/vscode/vscode.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.vscode = {
     enable = true;
@@ -6,8 +6,15 @@
 
     extensions = pkgs.my-vscode-extensions.allExtensions;
 
-    keybindings = import ./keybindings.nix;
+    # keybindings = import ./keybindings.nix;
+    # userSettings = import ./settings.nix;
+  };
 
-    userSettings = import ./settings.nix;
+  home.file.".config/VSCodium/User/settings.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/dotfiles/users/common/vscode/settings.json";
+  };
+
+  home.file.".config/VSCodium/User/keybindings.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/dotfiles/users/common/vscode/keybindings.json";
   };
 }
