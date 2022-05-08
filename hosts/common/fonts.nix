50dc6cee4d1d8c6ea0465d34c9c998180ea1f771
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-08 15:57:58 -0300

    feat(noto-fonts-cjk): add noto-fonts-cjk

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -4,6 +4,7 @@
     enableDefaultFonts = false;
     fonts = with pkgs; [
       noto-fonts
+      noto-fonts-cjk
       twitter-color-emoji
       (nerdfonts.override { fonts = [ "FiraCode" ]; })
       font-awesome_6
@@ -11,8 +12,8 @@
 
     fontconfig = {
       defaultFonts = {
-        sansSerif = [ "Noto Sans" ];
-        serif = [ "Noto Serif" ];
+        sansSerif = [ "Noto Sans" "Noto Sans CJK JP" "Noto Sans CJK SC" "Noto Sans CJK TC" ];
+        serif = [ "Noto Serif" "Noto Serif CJK JP" "Noto Serif CJK SC" "Noto Serif CJK TC" ];
         monospace = [ "FiraCode Nerd Font" ];
         emoji = [ "Twitter Color Emoji" ];
       };
