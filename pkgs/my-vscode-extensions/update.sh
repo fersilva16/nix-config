b48c3724c93aa4aa01c1821443d94b74359f3ad3
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-22 20:42:28 -0300

    feat(vscode-extensions): improve update script

--- pkgs/my-vscode-extensions/update.sh
+++ pkgs/my-vscode-extensions/update.sh
@@ -1,38 +1,33 @@
 #! /usr/bin/env bash
-# shellcheck shell=bash
 set -eu -o pipefail
 
-# Helper to just fail with a message and non-zero exit code.
 function fail() {
   echo "$1" >&2
   exit 1
 }
 
-# Helper to clean up after ourselves if we're killed by SIGINT.
 function clean_up() {
   TDIR="${TMPDIR:-/tmp}"
+
   echo "Script killed, cleaning up tmpdirs: $TDIR/vscode_exts_*" >&2
-  rm -Rf "$TDIR/vscode_exts_*"
+
+  rm -rf "$TDIR/vscode_exts_*"
 }
 
 function get_vsixpkg() {
   N="$1.$2"
 
-  # Create a tempdir for the extension download.
   EXTTMP=$(mktemp -d -t vscode_exts_XXXXXXXX)
 
   URL="https://$1.gallery.vsassets.io/_apis/public/gallery/publisher/$1/extension/$2/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
 
-  # Quietly but delicately curl down the file, blowing up at the first sign of trouble.
   curl --silent --show-error --retry 3 --fail -X GET -o "$EXTTMP/$N.zip" "$URL"
-  # Unpack the file we need to stdout then pull out the version
+
   VER=$(jq -r '.version' <(unzip -qc "$EXTTMP/$N.zip" "extension/package.json"))
-  # Calculate the SHA
+
   SHA=$(nix-hash --flat --base32 --type sha256 "$EXTTMP/$N.zip")
 
-  # Clean up.
-  rm -Rf "$EXTTMP"
-  # I don't like 'rm -Rf' lurking in my scripts but this seems appropriate.
+  rm -rf "$EXTTMP"
 
   cat <<-EOF
   {
@@ -44,19 +39,16 @@ function get_vsixpkg() {
 EOF
 }
 
-# Try to be a good citizen and clean up after ourselves if we're killed.
 trap clean_up SIGINT
 
-# Begin the printing of the nix expression that will house the list of extensions.
 printf '[\n'
 
-grep -v '^ *#' < extensions | while IFS= read -r line
+grep -v '^ *#' < extensions | while IFS= read -r LINE
 do
-  OWNER=$(echo "$line" | cut -d. -f1)
-  EXT=$(echo "$line" | cut -d. -f2)
+  OWNER=$(echo "$LINE" | cut -d. -f1)
+  EXT=$(echo "$LINE" | cut -d. -f2)
 
   get_vsixpkg "$OWNER" "$EXT"
 done
 
-# Close off the nix expression.
 printf ']\n'
