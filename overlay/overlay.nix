bb6f25d57db5ed2d259b492b85e23f7fd9ec5c43
Fernando Silva <fernando457829@gmail.com>
2022-06-08 20:43:17 -0300

    fix(discord): add overlay (#33)

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -2,4 +2,5 @@ self: super:
 {
   kitty-themes = import ./kitty-themes.nix self super;
   dmenu = import ./dmenu.nix self super;
+  discord = import ./discord.nix self super;
 } // import ../pkgs/pkgs.nix self
