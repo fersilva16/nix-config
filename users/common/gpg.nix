19bb755c18444fa8570dbf3b4db16f1ceaca70d4
Fernando Silva <fernando457829@gmail.com>
2022-06-13 12:52:15 -0300

    feat(gpg): use home-manager gpg-agent (#36)

--- users/common/gpg.nix
+++ users/common/gpg.nix
@@ -1,7 +1,15 @@
 _:
 {
-  home.file.".gnupg/gpg-agent.conf".text = ''
-    default-cache-ttl 14400
-    allow-emacs-pinentry
-  '';
+  services.gpg-agent = {
+    enable = true;
+
+    pinentryFlavor = "tty";
+
+    defaultCacheTtl = 86400;
+    maxCacheTtl = 86400;
+
+    extraConfig = ''
+      allow-emacs-pinentry
+    '';
+  };
 }
