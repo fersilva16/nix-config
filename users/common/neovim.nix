99388e5209d54df79ea55f6e54ba41fc5a8262e3
Fernando Silva <fernandonsilva16@gmail.com>
2022-06-24 10:07:02 -0300

    feat(neovim): add nvchad

--- users/common/neovim.nix
+++ users/common/neovim.nix
@@ -1,7 +1,26 @@
 { pkgs, ... }:
+let
+  nvchad = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "nvchad";
+    version = "2022-06-24";
+    src = pkgs.fetchFromGitHub {
+      owner = "NvChad";
+      repo = "NvChad";
+      rev = "5e4b2e6a117bd62a29f02b4825d1f1b1f2f21172";
+      sha256 = "sha256-zuBzkfm3FKWM/LDut5Ry+I6aNA0kfGpOHLyG1GDTaVc=";
+    };
+  };
+in
 {
-  home.packages = with pkgs; [
-    neovim
-    neovide
-  ];
+  programs.neovim = {
+    enable = true;
+
+    extraConfig = ''
+      source ${nvchad}/init.lua
+    '';
+
+    plugins = [
+      nvchad
+    ];
+  };
 }
