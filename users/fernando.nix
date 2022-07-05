b8534f761d3af44e34d599d2097ed6580298110e
Fernando Silva <fernandonsilva16@gmail.com>
2022-07-05 13:11:45 -0300

    feat(home): remove ticktick

--- users/fernando.nix
+++ users/fernando.nix
@@ -89,7 +89,6 @@
       ./common/rofi.nix
       ./common/slack.nix
       ./common/starship.nix
-      ./common/ticktick.nix
       ./common/vlc.nix
       ./common/vscode/vscode.nix
       ./common/wakatime.nix
