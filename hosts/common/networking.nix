79a4d3c3bb4c23c6a9770dc2da7d15a35e5acb1b
Fernando Silva <fernandonsilva16@gmail.com>
2022-07-13 11:18:39 -0300

    feat(networking): remove extra hosts

--- hosts/common/networking.nix
+++ hosts/common/networking.nix
@@ -4,12 +4,4 @@ _:
     networkmanager.enable = true;
     useDHCP = false;
   };
-
-  networking.extraHosts = ''
-    127.0.0.1 company1
-    127.0.0.1 company2
-    127.0.0.1 company3
-    127.0.0.1 company4
-    127.0.0.1 company5
-  '';
 }
