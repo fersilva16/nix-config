e74a2dae014339e29dd171b5347692185eaca2b6
Fernando Silva <fernandonsilva16@gmail.com>
2022-07-14 10:19:46 -0300

    feat(networking): add extra hosts

--- hosts/common/networking.nix
+++ hosts/common/networking.nix
@@ -4,4 +4,12 @@ _:
     networkmanager.enable = true;
     useDHCP = false;
   };
+
+  networking.extraHosts = ''
+    127.0.0.1 company1
+    127.0.0.1 company2
+    127.0.0.1 company3
+    127.0.0.1 company4
+    127.0.0.1 company5
+  '';
 }
