eb039cc2faca497c6d396cedbe361eeca98ff9cc
Fernando Silva <fernandonsilva16@gmail.com>
2022-07-30 13:42:47 -0300

    feat(fonts): remove font-awesome v6 overlay

--- pkgs/pkgs.nix
+++ pkgs/pkgs.nix
@@ -1,7 +1,5 @@
 pkgs:
 {
-  font-awesome_6 = pkgs.callPackage ./font-awesome_6/font-awesome_6.nix { };
-
   my-vscode-extensions = pkgs.callPackage ./my-vscode-extensions/my-vscode-extensions.nix { };
 
   responsively = pkgs.callPackage ./responsively.nix { };
