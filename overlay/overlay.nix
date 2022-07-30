3877fb8d70468cc83a3353ab8b789115f95beae9
Fernando Silva <fernandonsilva16@gmail.com>
2022-07-30 16:34:11 -0300

    feat(overlay): remove kitty-themes overlay

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -1,6 +1,5 @@
 self: super:
 {
-  kitty-themes = import ./kitty-themes.nix self super;
   dmenu = import ./dmenu.nix self super;
   discord = import ./discord.nix self super;
   noto-fonts-cjk-sans = import ./noto-fonts-cjk-sans.nix self super;
