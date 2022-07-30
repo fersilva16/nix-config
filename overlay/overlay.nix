cd1de4664e327a236ff800e4274d960eeaa1ca37
Fernando Silva <fernandonsilva16@gmail.com>
2022-07-30 16:42:15 -0300

    feat(overlay): remove discord overlay

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -1,7 +1,6 @@
 self: super:
 {
   dmenu = import ./dmenu.nix self super;
-  discord = import ./discord.nix self super;
   noto-fonts-cjk-sans = import ./noto-fonts-cjk-sans.nix self super;
   noto-fonts-cjk-serif = import ./noto-fonts-cjk-serif.nix self super;
 } // import ../pkgs/pkgs.nix self
