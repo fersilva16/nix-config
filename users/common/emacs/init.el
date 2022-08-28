388b333c52e92195d9148ee9a876468fc98fbe85
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-27 23:47:03 -0300

    feat(emacs): add local leader

--- users/common/emacs/init.el
+++ users/common/emacs/init.el
@@ -228,7 +228,6 @@ NAME, ARGLIST, and BODY are the same as `defun', `defun*', `defmacro', and
  :prefix "SPC"
  ":" 'execute-extended-command
  "f o" 'find-file
- "f e" 'eval-buffer
  "f s" 'save-buffer
  "b b" 'switch-to-buffer
  "b k" 'kill-current-buffer
@@ -244,3 +243,11 @@ NAME, ARGLIST, and BODY are the same as `defun', `defun*', `defmacro', and
  "n d d" 'org-roam-dailies-goto-date
  "h" help-map)
 
+(general-create-definer localleader-def
+  :prefix "SPC m")
+
+(localleader-def
+  :states 'normal
+  :keymaps emacs-lisp-mode-map
+  "e" '(:ignore t :wk "eval")
+  "eb" #'eval-buffer)
