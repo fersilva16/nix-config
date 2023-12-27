466620abec98251eadff0a0200918d5e39c60f03
Fernando Silva <fernandonsilva16@gmail.com>
2023-12-26 23:36:30 -0300

    feat(pkgs): remove unused pkgs

--- pkgs/pkgs.nix
+++ pkgs/pkgs.nix
@@ -1,10 +1,4 @@
 pkgs:
 {
   my-vscode-extensions = pkgs.callPackage ./my-vscode-extensions/my-vscode-extensions.nix { };
-
-  responsively = pkgs.callPackage ./responsively.nix { };
-
-  mov-cli = pkgs.callPackage ./mov-cli.nix { };
-
-  bookcut = pkgs.callPackage ./bookcut.nix { };
 }
