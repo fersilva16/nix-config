ea93371cfd295bec3fd07ddbb00e3c6b6f9af680
Fernando Silva <fernandonsilva16@gmail.com>
2024-06-28 11:56:22 -0300

    feat(overlay): reorganize

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -1,7 +1,2 @@
 self: super:
-{
-  discord = import ./discord.nix self super;
-  dmenu = import ./dmenu.nix self super;
-  noto-fonts-cjk-sans = import ./noto-fonts-cjk-sans.nix self super;
-  noto-fonts-cjk-serif = import ./noto-fonts-cjk-serif.nix self super;
-} // import ../pkgs/pkgs.nix self
+{ } // import ./pkgs/pkgs.nix self
