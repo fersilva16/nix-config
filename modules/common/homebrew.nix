59327bd2d3ac4d2972416d5763161220548d7d5e
Fernando Silva <fernando457829@gmail.com>
2024-10-31 14:22:52 -0300

    feat(m1): add libreoffice (#228)

--- modules/common/homebrew.nix
+++ modules/common/homebrew.nix
@@ -13,7 +13,7 @@ in
     taps = {
       "homebrew/homebrew-core" = homebrew-core;
       "homebrew/homebrew-cask" = homebrew-cask;
-      "homebrew/bundle" = homebrew-bundle;
+      "homebrew/homebrew-bundle" = homebrew-bundle;
     };
 
     mutableTaps = false;
