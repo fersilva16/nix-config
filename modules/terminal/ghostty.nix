a9aae47303c4fbeaccedf161a92a391c859589e6
Fernando Silva <fernando457829@gmail.com>
2026-03-09 14:46:40 -0300

    feat(m1): add ghostty (#299)

---
+++ modules/terminal/ghostty.nix
@@ -0,0 +1,31 @@
+{ username, pkgs, ... }:
+{
+  home-manager.users.${username} = {
+    programs.ghostty = {
+      enable = true;
+      enableFishIntegration = true;
+      package = pkgs.ghostty-bin;
+
+      settings = {
+        theme = "Flexoki Light";
+
+        font-family = "CaskaydiaCove Nerd Font";
+        font-family-bold = "CaskaydiaCove Nerd Font";
+        font-family-italic = "auto";
+        font-family-bold-italic = "auto";
+        font-size = 12;
+
+        cursor-style = "bar";
+        cursor-style-blink = true;
+        adjust-cursor-thickness = 2;
+
+        macos-titlebar-style = "transparent";
+        window-theme = "auto";
+        gtk-titlebar = false;
+
+        window-padding-x = 8;
+        window-padding-y = 8;
+      };
+    };
+  };
+}
