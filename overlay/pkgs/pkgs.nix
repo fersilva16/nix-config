c45ebeac7c9828f1d49ad7fa258b0fa532aba534
Fernando Silva <fernando457829@gmail.com>
2026-03-10 14:23:01 -0300

    feat(tmux): nerd font window name (#316)

--- overlay/pkgs/pkgs.nix
+++ overlay/pkgs/pkgs.nix
@@ -2,4 +2,7 @@ pkgs: {
   paisa = pkgs.callPackage ./paisa.nix { };
   flexoki-tmux = pkgs.callPackage ./flexoki-tmux/flexoki-tmux.nix { };
   tmux-extras = pkgs.callPackage ./tmux-extras/tmux-extras.nix { };
+  tmux-nerd-font-window-name =
+    pkgs.callPackage ./tmux-nerd-font-window-name/tmux-nerd-font-window-name.nix
+      { };
 }
