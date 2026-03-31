7ebc533a4f1f7df7ebd237554f1d1477651e75e2
Fernando Silva <fernando457829@gmail.com>
2026-03-30 23:15:23 -0300

    feat(nvim): add neo tree (#386)

--- modules/editor/nvim.nix
+++ modules/editor/nvim.nix
@@ -238,8 +238,8 @@ in
             '  Buffers & Tabs',
             '  Shift+h / Shift+l  prev / next buffer',
             '  Space bd           delete buffer',
-            '  Space ett          new tab',
-            '  Space etc          close tab',
+            '  Space Tt           new tab',
+            '  Space Tc           close tab',
             s,
             '  Splits & Windows',
             '  Space sv           vertical split',
@@ -279,6 +279,11 @@ in
             '  go / goo           send range/line to opencode',
             s,
             '  File Explorer',
+            '  Space e            toggle sidebar (right)',
+            '  Space ;            focus sidebar',
+            '  Space Ef           sidebar: file tree',
+            '  Space Eg           sidebar: git status',
+            '  Space Eb           sidebar: buffers',
             '  Space oo           open Oil',
             '  -  (in Oil)        go up directory',
             s,
@@ -606,14 +611,34 @@ in
                 { "<leader>csd", function() require('telescope.builtin').lsp_document_symbols() end, desc = "Document symbols" },
                 { "<leader>csw", function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end, desc = "Workspace symbols" },
 
-                -- Editor
-                { "<leader>e", group = "editor" },
-                { "<leader>et", group = "tabs" },
-                { "<leader>ett", "<cmd>tabnew<cr>", desc = "New tab" },
-                { "<leader>etc", "<cmd>tabclose<cr>", desc = "Close tab" },
-                { "<leader>eto", "<cmd>tabonly<cr>", desc = "Only this tab" },
-                { "<leader>etp", "<cmd>tabprevious<cr>", desc = "Previous tab" },
-                { "<leader>etn", "<cmd>tabnext<cr>", desc = "Next tab" },
+                -- Explorer (neo-tree)
+                { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle file explorer" },
+                { "<leader>;", function()
+                    local cur_buf = vim.api.nvim_get_current_buf()
+                    if vim.bo[cur_buf].filetype == 'neo-tree' then
+                      vim.cmd('wincmd p')
+                    else
+                      for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        local buf = vim.api.nvim_win_get_buf(win)
+                        if vim.bo[buf].filetype == 'neo-tree' then
+                          vim.api.nvim_set_current_win(win)
+                          return
+                        end
+                      end
+                    end
+                  end, desc = "Toggle focus file explorer" },
+                { "<leader>E", group = "explorer views" },
+                { "<leader>Ef", "<cmd>Neotree toggle reveal right<cr>", desc = "File tree" },
+                { "<leader>Eg", "<cmd>Neotree toggle git_status right<cr>", desc = "Git status" },
+                { "<leader>Eb", "<cmd>Neotree toggle buffers right<cr>", desc = "Buffers" },
+
+                -- Tabs
+                { "<leader>T", group = "tabs" },
+                { "<leader>Tt", "<cmd>tabnew<cr>", desc = "New tab" },
+                { "<leader>Tc", "<cmd>tabclose<cr>", desc = "Close tab" },
+                { "<leader>To", "<cmd>tabonly<cr>", desc = "Only this tab" },
+                { "<leader>Tp", "<cmd>tabprevious<cr>", desc = "Previous tab" },
+                { "<leader>Tn", "<cmd>tabnext<cr>", desc = "Next tab" },
 
                 -- Splits (VS Code-like)
                 { "<leader>sv", "<cmd>vsplit<cr>", desc = "Vertical split" },
@@ -787,6 +812,13 @@ in
                       text = 'File Explorer',
                       text_align = 'center',
                     },
+                    {
+                      filetype = 'neo-tree',
+                      text = 'Explorer',
+                      text_align = 'center',
+                      highlight = 'Directory',
+                      separator = true,
+                    },
                   },
                 },
               })
@@ -889,6 +921,113 @@ in
                 use_default_keymaps = true,
                 view_options = {
                   show_hidden = true,
+                },
+              })
+            EOF
+          '';
+        }
+
+        # ── Sidebar file tree + git status ─────────────────
+        vimPlugins.nui-nvim
+        {
+          plugin = vimPlugins.neo-tree-nvim;
+          config = ''
+            lua << EOF
+              -- Open neo-tree on startup
+              vim.api.nvim_create_autocmd('VimEnter', {
+                callback = function()
+                  -- Only open if we're not opening a specific file via stdin or with args that are directories
+                  if vim.fn.argc() == 0 then return end
+                  vim.cmd('Neotree show right')
+                end,
+              })
+
+              -- Also open when entering a buffer (covers opening nvim with a file)
+              vim.api.nvim_create_autocmd('BufReadPost', {
+                once = true,
+                callback = function()
+                  vim.schedule(function()
+                    vim.cmd('Neotree show right')
+                  end)
+                end,
+              })
+
+              require('neo-tree').setup({
+                close_if_last_window = true,
+                popup_border_style = 'rounded',
+                enable_git_status = true,
+                enable_diagnostics = true,
+                sort_case_insensitive = true,
+
+                -- Sidebar on the right
+                default_component_configs = {
+                  indent = {
+                    indent_size = 2,
+                    with_markers = true,
+                    indent_marker = "│",
+                    last_indent_marker = "└",
+                  },
+                  icon = {
+                    folder_closed = "",
+                    folder_open = "",
+                    folder_empty = "",
+                  },
+                  git_status = {
+                    symbols = {
+                      added     = "✚",
+                      modified  = "",
+                      deleted   = "✖",
+                      renamed   = "󰁕",
+                      untracked = "",
+                      ignored   = "",
+                      unstaged  = "󰄱",
+                      staged    = "",
+                      conflict  = "",
+                    },
+                  },
+                },
+
+                window = {
+                  position = 'right',
+                  width = 36,
+                  mappings = {
+                    ['<space>'] = 'none', -- don't conflict with leader
+                  },
+                },
+
+                filesystem = {
+                  follow_current_file = { enabled = true },
+                  hijack_netrw_behavior = 'disabled', -- Oil handles netrw
+                  use_libuv_file_watcher = true,
+                  filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    never_show = { '.DS_Store' },
+                  },
+                },
+
+                git_status = {
+                  window = {
+                    position = 'right',
+                    mappings = {
+                      ['<space>'] = 'none',
+                    },
+                  },
+                  renderers = {
+                    file = {
+                      { "indent" },
+                      { "icon" },
+                      { "git_status", highlight = "NeoTreeDimText" },
+                      { "name" },
+                      { "diagnostics" },
+                    },
+                  },
+                },
+
+                buffers = {
+                  follow_current_file = { enabled = true },
+                  window = { position = 'right' },
                 },
               })
             EOF
