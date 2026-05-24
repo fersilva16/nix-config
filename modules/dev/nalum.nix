a7f80de93cf662cfb51712a073e11708f7093d7a
Fernando Silva <fernandonsilva16@gmail.com>
2026-05-23 23:23:56 -0300

    Revert "fix(nalum): use bare-string activation form"
    This reverts commit e89c4b571c4cea3820f9d543dd199fd941ba7992.

--- modules/dev/nalum.nix
+++ modules/dev/nalum.nix
@@ -20,7 +20,7 @@ mkUserModule {
   name = "nalum";
 
   home =
-    { username, ... }:
+    { lib, username, ... }:
     let
       nalumDir = "/Users/${username}/nalum";
     in
@@ -28,7 +28,7 @@ mkUserModule {
       home = {
         packages = [ inputs.hermes-agent.packages.${pkgs.system}.default ];
         sessionVariables.HERMES_HOME = nalumDir;
-        activation.nalumCheck = ''
+        activation.nalumCheck = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
           if [ ! -d "${nalumDir}" ]; then
             echo ""
             echo "  ⚠ nalum: ${nalumDir} does not exist."
