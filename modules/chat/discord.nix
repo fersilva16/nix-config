9d553050a1b5d29dc17228d82a89b0fb618b2dfc
Fernando Silva <fernandonsilva16@gmail.com>
2026-07-19 04:11:19 +0000

    feat(polaris): add discord

--- modules/chat/discord.nix
+++ modules/chat/discord.nix
@@ -1,5 +1,13 @@
-{ mkUserModule, ... }:
+{
+  mkUserModule,
+  forPlatform,
+  pkgs,
+  ...
+}:
 mkUserModule {
   name = "discord";
   casks = [ "discord" ];
+  home = {
+    home.packages = forPlatform { linux = [ pkgs.discord ]; };
+  };
 }
